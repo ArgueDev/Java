79a107b159ead2e83441f817b14b1ee34f62b35a
ArgueDev <christianarguelloo97@gmail.com>
2026-03-23 10:51:48 -0500

    Metodos Staticos

--- Seccion14/src/Automovil.java
+++ Seccion14/src/Automovil.java
@@ -1,15 +1,98 @@
 public class Automovil {
 
-    String fabricante;
-    String modelo;
-    String color = "Gris";
-    double cilindraje;
+    private String fabricante;
+    private String modelo;
+    private String color = "Gris";
+    private double cilindraje;
+    private int capacidadTanque = 40;
 
-    public void detalle() {
+    private static int matricula;
+
+    public static int getMatricula() {
+        return matricula;
+    }
+
+    public static void setMatricula(int matricula) {
+        Automovil.matricula = matricula;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindraje, int capacidadTanque) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+        this.cilindraje = cilindraje;
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(double cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    public int getCapacidadTanque() {
+        return capacidadTanque;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" +
+                "fabricante='" + fabricante + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", cilindraje=" + cilindraje +
+                ", capacidadTanque=" + capacidadTanque +
+                '}';
+    }
+
+    public void verDetalle() {
         System.out.println("Fabricante = " + this.fabricante);
         System.out.println("Modelo = " + this.modelo);
         System.out.println("Color = " + this.color);
         System.out.println("Cilindraje = " + this.cilindraje);
+        System.out.println("Capacidad de Tanque = " + this.capacidadTanque);
+    }
+
+    public String acelerar(int rpm) {
+        return "El auto " + this.fabricante + " acelerando a " + rpm + " rpm";
+    }
+
+    public String frenar() {
+        return this.fabricante + " " + this.modelo + " frenando!";
+    }
+
+    public float calcularConsumo(int km, float porcentaje) {
+        return km / (porcentaje * this.capacidadTanque);
     }
 
 }
--- Seccion14/src/EjemploAutomovil.java
+++ Seccion14/src/EjemploAutomovil.java
@@ -1,19 +1,37 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
-        Automovil subaru = new Automovil();
-        subaru.fabricante = "Subaru";
-        subaru.modelo = "Impresa";
-        subaru.cilindraje = 2.0;
-        subaru.color = "Rojo";
-        subaru.detalle();
+//        Automovil subaru = new Automovil();
+//        subaru.setFabricante("Subaru");
+//        subaru.setColor("Rojo");
+//        subaru.setModelo("Impreza");
+//        subaru.setCapacidadTanque(3000);
+//        subaru.setCilindraje(2.0);
+//        subaru.verDetalle();
 
-        Automovil mazda = new Automovil();
-        mazda.fabricante = "Mazda";
-        mazda.modelo = "BT-50";
-        mazda.cilindraje = 3.0;
-        mazda.color = "Negro";
-        mazda.detalle();
+//        Automovil mazda = new Automovil();
+//        mazda.fabricante = "Mazda";
+//        mazda.modelo = "BT-50";
+//        mazda.cilindraje = 3.0;
+//        mazda.color = "Negro";
+//        mazda.verDetalle();
+//        System.out.println(mazda.acelerar(3000));
+//        System.out.println(mazda.frenar());
+//        System.out.println("Kilometraje por litro = " + mazda.calcularConsumo(300, 0.6f));
+
+        Automovil skoda = new Automovil(
+                "Skoda",
+                "Octavia",
+                "Plomo",
+                2.0,
+                3000
+        );
+
+        skoda.verDetalle();
+        Automovil.setMatricula(2025);
+        System.out.println("Matricula = " + Automovil.getMatricula());
+
+        System.out.println(skoda);
 
     }
 }
